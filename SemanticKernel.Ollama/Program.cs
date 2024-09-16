7642e84b97d95336de67445d97ec8273c8b46876
Matteo Pagani <matteo.pagani@microsoft.com>
2024-09-16 13:42:33 +0200

    Added ollama example

---
+++ SemanticKernel.Ollama/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
+
+var chatService = new OllamaChatCompletionService(modelId: "phi3", endpoint: new Uri("http://localhost:11434"));
+
+var chatHistory = new ChatHistory("You are a helpful assistant that knows about AI.");
+
+string message = string.Empty;
+while (message != "stop")
+{
+    message = Console.ReadLine();
+    if (message != "stop")
+    {
+        chatHistory.AddUserMessage(message);
+        var reply = await chatService.GetChatMessageContentAsync(chatHistory);
+        chatHistory.AddAssistantMessage(reply.Content);
+        Console.WriteLine(reply.Content);
+    }
+}
